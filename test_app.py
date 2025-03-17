8b62046cc9089a0d64d4d0474e51cb5bed195a3d
IDGS-801-21000421 <murilloabraham976@gmail.com>
2025-03-17 15:38:12 -0600

    Corrección de CI/CD para despliegue en Heroku

---
+++ test_app.py
@@ -0,0 +1,7 @@
+from app import app
+
+def test_status():
+    client = app.test_client()
+    response = client.get('/status')
+    assert response.status_code == 200
+    assert response.json == {"message": "Microservicio en ejecución"}
